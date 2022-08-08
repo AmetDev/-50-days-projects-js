df0ab9e3e0d0460cc5dee01cd34150198594b158
Amet <ametsejdaliev@gmail.com>
2022-08-08 11:46:01 +0300

    beta-test

---
+++ dropdown-made-on-linux/script.js
@@ -0,0 +1,8 @@
+const btn = document.querySelectorAll(".button")
+console.log(btn)
+
+//btn.addEventListener("click", function() {
+//    btn.forEach((e) => {
+//        console.log(e)
+//    })
+//})
