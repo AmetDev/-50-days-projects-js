3de0ae6a9fad4cd9d7d80fe838bbb56fdd24ffa4
AmetJunior <ametsejdaliev@gmail.com>
2022-08-25 16:42:29 +0300

    main

---
+++ activebox1/script.js
@@ -0,0 +1,8 @@
+const BtnRed = document.getElementById("btn-red")
+console.log(BtnRed)
+
+BtnRed.addEventListener("click", function() {
+    const intro = document.querySelector(".intro")
+    // console.log(intro)
+    // intro.classList.add("overlay_modal")
+})
