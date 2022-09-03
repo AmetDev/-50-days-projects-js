bc967d32118291e4f30302f48412b02ceecc0cf1
AmetJunior <ametsejdaliev@gmail.com>
2022-09-03 18:54:10 +0300

    jsss

--- 12.01-forma-mail/js/script.js
+++ 12.01-forma-mail/js/script.js
@@ -3,9 +3,9 @@
 document.addEventListener('DOMContentLoaded', function () {
   const form = document.querySelector("form");
   console.log(form)
-  // const telSelector = form.querySelector('input[type="tel"]');
-  // const inputMask = new Inputmask('+7 (999) 999-99-99');
-  // inputMask.mask(telSelector);
+  const telSelector = form.querySelector('input[type="tel"]');
+  const inputMask = new Inputmask('+7 (999) 999-99-99');
+  inputMask.mask(telSelector);
 
   form.addEventListener('sumbit', formSend);
 
