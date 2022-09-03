9e4ce9bea85377af2669342fe66a4162c99f783a
AmetJunior <ametsejdaliev@gmail.com>
2022-09-03 18:50:46 +0300

    commit-test-for-linux

--- 12.01-forma-mail/js/script.js
+++ 12.01-forma-mail/js/script.js
@@ -1,4 +1,16 @@
-const form = document.querySelector('.form');
-const telSelector = form.querySelector('input[type="tel"]');
-const inputMask = new Inputmask('+7 (999) 999-99-99');
-inputMask.mask(telSelector);
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.querySelector("form");
+  console.log(form)
+  // const telSelector = form.querySelector('input[type="tel"]');
+  // const inputMask = new Inputmask('+7 (999) 999-99-99');
+  // inputMask.mask(telSelector);
+
+  form.addEventListener('sumbit', formSend);
+
+  async function formSend(e) {
+    e.preventDefault();
+  }
+
+})
