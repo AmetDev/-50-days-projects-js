778de827ecab206b9fe474e933ba6a5f7c815ab4
AmetJunior <ametsejdaliev@gmail.com>
2022-09-17 15:19:29 +0300

    gg-valudate

--- 12.01-forma-mail/js/script.js
+++ 12.01-forma-mail/js/script.js
@@ -1,20 +1,27 @@
 
 
 document.addEventListener('DOMContentLoaded', function () {
-  const form = document.getElementById("form")
-  form.addEventListener('click', formSend);
+  const form = document.getElementById("form");
+  form.addEventListener('submit', formSend);
 
    async function formSend(e) {
     e.preventDefault();
-    let inputValudate = document.querySelectorAll("._req")
+    let inputValudate = document.querySelectorAll("._req");
     inputValudate.forEach(function(element) {
-      let inputValue  = element.value
-      if(inputValue === "") {
-        element.classList.add("error")
-          if (element.classList.contains("error")) {
-            element.classList.add("_style_error")
+    let inputValue  = element.value
+      if(inputValue == "") {
+          element.classList.add("_style_error")
+        } else {
+          element.classList.remove("_style_error")   
+        };
+      const emailInput = document.querySelector(".input-mail")
+      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailInput)) {
+        emailInput.classList.remove("_style_error")
+        console.log(true)
+      } else { 
+        console.log(false)
+        emailInput.classList.add("_style_error")
       }
-      }
-    })
-  }
-})
+    });
+  };
+});
